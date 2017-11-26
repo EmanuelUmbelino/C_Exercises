ac43b821ec6b1808e686643b89803dd30d1bdda8
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2017-11-26 13:14:20 -0200

    Exercicio Fila - 0.8
    - Implementada funcao separa filas

--- Fila/main.c
+++ Fila/main.c
@@ -3,7 +3,15 @@
 
 void separa_filas(Fila *f, Fila *f_pares, Fila *f_impares)
 {
-
+    int v;
+    while(!vazia(f))
+    {
+        v = retira(f);
+        if(v%2==0)
+            insere(f_pares,v);
+        else
+            insere(f_impares,v);
+    }
 }
 
 int main()

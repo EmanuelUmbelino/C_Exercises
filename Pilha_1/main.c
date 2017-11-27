9bf95a064e804aae9b6a4b57a91b8e677237e4ab
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2017-11-27 01:27:30 -0200

    Exercicio Pilha - 0.3
    - Cotinuando funcao soma grandes numeros

--- Pilha_1/main.c
+++ Pilha_1/main.c
@@ -4,7 +4,7 @@
 void somaGrandesNumeros()
 {
     Pilha *p1 = piCria(), *p2 = piCria(), *r1 = piCria();
-    int aux;
+    int aux, aux1, aux2;
     printf("\nDigite cada algarismo do primeiro numero (termina com numero negativo):\n");
     while(1)
     {
@@ -21,10 +21,18 @@ void somaGrandesNumeros()
             break;
         piPush(p2,aux);
     }
-
+    aux = 0;
     while(!piEhVazia(p1) || !piEhVazia(p1))
     {
-
+        aux += aux1;
+        aux1 = piPop(p1);
+        aux2 = piPop(p2);
+        aux = aux1 + aux2;
+        aux1 = aux % 10;
+        if(aux < 10)
+            piPush(r1,aux);
+        else
+            piPush(r1,aux/10);
     }
 
     piLibera(p1);piLibera(p2);piLibera(r1);

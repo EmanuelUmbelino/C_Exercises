cfb9ab4963e86f0eacb12bb03727572dcabcb68d
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2017-11-28 14:49:45 -0200

    Exercicio TAD 3 - Final
    - Funcao de busca binaria

--- TAD_3/main.c
+++ TAD_3/main.c
@@ -50,6 +50,29 @@ int contaPrimeiroQuadrante(No *lst)
     return noPrimeiroQuadrante(lst->info) + contaPrimeiroQuadrante(lst->prox);
 }
 
+char *buscaBinaria(Quadrado **q, int n, float x, float y)
+{
+    int ini = 0, fim = n-1;
+    while(ini <= fim)
+    {
+        int meio = (ini+fim)/2;
+        if (x < quaObtemXdeVSD(q[meio]))
+            fim = meio-1;
+        else if (x > quaObtemXdeVSD(q[meio]))
+            ini = meio+1;
+        else
+        {
+            if (y < quaObtemYdeVSD(q[meio]))
+                fim = meio-1;
+            else if (y > quaObtemYdeVSD(q[meio]))
+                ini = meio+1;
+            else
+                return quaObtemNome(q[meio]);
+        }
+    }
+    return NULL;
+}
+
 int main()
 {
     No *lst = criaLst();
@@ -58,10 +81,19 @@ int main()
     lst = addNo(lst,5,5,5,"C");
     lst = addNo(lst,15,15,5,"D");
     lst = addNo(lst,0,-2,5,"E");
-    printf("%d",contaPrimeiroQuadrante(lst));
+    printf("%d estao no 1o quadrante",contaPrimeiroQuadrante(lst));
     liberaLst(lst);
+    
+    int i, n = 5;
+    Quadrado *v[n];
+    v[0] = quaCria(-2,0,15,"B");
+    v[1] = quaCria(0,-2,5,"E");
+    v[2] = quaCria(5,0,5,"A");
+    v[3] = quaCria(5,5,5,"C");
+    v[4] = quaCria(15,15,5,"D");
+    printf("\nO carinha da busca: ");
+    printf(buscaBinaria(v, n, 5, 5));
+    for(i = 0; i < n; i++)
+        quaLibera(v[i]);
     return 0;
 }
-
-
-
